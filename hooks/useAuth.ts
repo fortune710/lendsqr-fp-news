dfa1adc7d3d97cd4dcc168ab87cd578c8b97429e
fortune710 <fortune0208@yahoo.com>
2023-11-07 12:18:58 +0100

    added crashlytics to sign up functions

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,10 +1,13 @@
 import auth from "@react-native-firebase/auth"
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import useCrashlytics from "./useCrashlytics";
 
 const useAuth = () => {
-    
+    const crashlytics = useCrashlytics();
+
     const emailAndPasswordSignUp = async (email: string, password: string) => {
         const { user } = await auth().createUserWithEmailAndPassword(email, password);
+        crashlytics.log(`User with ID ${user.uid} signed up`);
         return user;
     }
 
@@ -12,7 +15,12 @@ const useAuth = () => {
         GoogleSignin.configure({
             webClientId: "394802504165-pdja8oh5th7u8etftvsih9hs2cpksa48.apps.googleusercontent.com",
         })
-        await GoogleSignin.signIn()
+        const user = await GoogleSignin.signIn();
+        crashlytics.log(`User with ID ${user.user.email} signed in with Google`);
     }
-    return auth()
+    return {
+        auth: auth(),
+        googleSignIn,
+        emailAndPasswordSignUp
+    }
 }
