ef60eff9d3692be17e84738473ec24ecf72e9876
fortune710 <fortune0208@yahoo.com>
2023-11-07 11:12:34 +0100

    added new pages to stcak navigator

--- navigation/StackNavigator.tsx
+++ navigation/StackNavigator.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View } from "react-native";
+
 import NewsFeed from "../pages/NewsFeed";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import NewsDetails from "../pages/NewsDetails";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +14,13 @@ const StackNavigator = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="" component={NewsFeed} />
+                <Stack.Screen name="News-Listing" component={NewsFeed} />
+                <Stack.Screen name="News-Details" component={NewsDetails} />
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Sign-Up" component={SignUp} />
             </Stack.Navigator>
         </NavigationContainer>
     )
 }
+
+export default StackNavigator;
