bebd4bfada6c1ebdd896f38c86680941dfe268bf
fortune710 <fortune0208@yahoo.com>
2023-11-09 01:11:00 +0100

    added google sign in

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,8 +1,53 @@
-import { View } from "react-native"
+import { Pressable, TextInput, View } from "react-native"
+import Page from "../components/Page"
+import { ScreenProps } from "../types";
+import { Button, Icon, Text } from "@rneui/themed";
+import useAuth from "../hooks/useAuth";
+import Input from "../components/Input";
 
-const Login = () => {
+
+const Login: React.FC<ScreenProps> = ({ navigation }) => {
+    const { googleSignIn } = useAuth();
+
+    const signUpWithGoogle = async () => {
+        await googleSignIn();
+        return navigation.navigate("News-Listing")
+    }
+
     return (
-        <View></View>
+        <Page>
+
+            <View>
+                <Input
+                    label="Email"
+                    placeholder="Enter you email"
+                />
+
+                <Input
+                    label="Password"
+                    placeholder="Enter you password"
+                    secureTextEntry={true}
+                />
+            
+            </View>
+
+            <Button onPress={signUpWithGoogle}>
+                Sign In with Google
+            </Button>
+
+            <Button onPress={() => navigation.navigate("News-Listing")}>
+                Go to feed
+            </Button>
+
+
+            <View style={{ display: "flex", alignItems: "center", flexDirection: "row" }}>
+                <Text>Don't have an account?</Text>
+                <Pressable onPress={() => navigation.navigate("Sign-Up")}>
+                    <Text>Sign Up</Text>
+                </Pressable>
+            </View>
+
+        </Page>
     )
 }
 
--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -1,12 +1,44 @@
-import { View } from "react-native"
+import { Pressable, View } from "react-native"
 import Page from "../components/Page";
-import { Input } from "@rneui/themed";
+import { Button, Icon, Input, Text } from "@rneui/themed";
+import useAuth from "../hooks/useAuth";
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
 
-const SignUp = () => {
+interface SignUpProps {
+    navigation: NavigationProp<ParamListBase>;
+};
+  
+
+const SignUp: React.FC<SignUpProps> = ({ navigation }) => {
+
+    const { googleSignIn } = useAuth();
+
+    const signUpWithGoogle = async () => {
+        await googleSignIn();
+        return navigation.navigate("News-Listing")
+    }
+
     return (
         <Page>
             <View>
                 <Input label="Email"/>
+
+                <Button onPress={signUpWithGoogle}>
+                    <Icon 
+                        type="ionicons"
+                        name="logo-google" 
+                        color={"white"}
+                    />
+                    Sign Up with Google
+                </Button>
+
+                <View style={{ display: "flex", alignItems: "center", flexDirection: "row" }}>
+                    <Text>Already have an account?</Text>
+                    <Pressable onPress={() => navigation.navigate("Login")}>
+                        <Text>Login</Text>
+                    </Pressable>
+                </View>
+
 
             </View>
         </Page>
