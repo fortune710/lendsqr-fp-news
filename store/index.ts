381e3c146b9941d89a4ece6da8cc7d7fe1e256dc
fortune710 <fortune0208@yahoo.com>
2023-11-09 01:13:46 +0100

    redux store configuration with news slice

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { NewsItem } from "../types";
 
-let reducer: any
+interface NewsState {
+  news: NewsItem[];
+}
+
+const initialState: NewsState = {
+  news: [],
+};
+
+const newsSlice = createSlice({
+  name: 'news',
+  initialState,
+  reducers: {
+    storeNews: (state, action: PayloadAction<NewsItem[]>) => {
+      state.news = action.payload;
+    },
+  },
+});
+
+export const selectNewsByTitle = (state: any, title: string) =>
+  state.news.news.find((news: NewsItem) => news.title === title);
+
+export const { storeNews } = newsSlice.actions;
 
 const store = configureStore({
-    reducer: reducer
-})
+  reducer: {
+    news: newsSlice.reducer,
+  },
+});
 
 export default store;
