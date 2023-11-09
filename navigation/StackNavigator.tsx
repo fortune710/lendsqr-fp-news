d6e57056d51e5cf6373ca2267de6ec0a163782f3
fortune710 <fortune0208@yahoo.com>
2023-11-09 01:12:23 +0100

    Add useAuth hook to StackNavigator to check if user if authenticated

--- navigation/StackNavigator.tsx
+++ navigation/StackNavigator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View } from "react-native";
@@ -7,13 +7,19 @@ import NewsFeed from "../pages/NewsFeed";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import NewsDetails from "../pages/NewsDetails";
+import useAuth from "../hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
+    const { auth: { currentUser, onAuthStateChanged } } = useAuth();
+
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator
+                screenOptions={{ headerShown: false }}
+                initialRouteName={!currentUser ? "Login" : "News-Listing"}
+            >
                 <Stack.Screen name="News-Listing" component={NewsFeed} />
                 <Stack.Screen name="News-Details" component={NewsDetails} />
                 <Stack.Screen name="Login" component={Login} />
