1ce7531550c03233a21bcdf40a9160311b783781
fortune710 <fortune0208@yahoo.com>
2023-11-11 02:12:59 +0100

    restyled page

--- pages/NewsDetails.tsx
+++ pages/NewsDetails.tsx
@@ -3,10 +3,13 @@ import Page from "../components/Page";
 import { useSelector } from "react-redux";
 import { selectNewsByTitle } from "../store";
 import { NewsItem, ScreenProps } from "../types";
-import { Icon, Text } from "@rneui/themed";
+import { Avatar, Icon, Text } from "@rneui/themed";
 import useCrashlytics from "../hooks/useCrashlytics";
 import { useEffect } from "react";
 import useAuth from "../hooks/useAuth";
+import { formatDistanceToNow, parseISO } from "date-fns";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
 
 const NewsDetails: React.FC<ScreenProps> = ({ route, navigation }) => {
 
@@ -15,6 +18,8 @@ const NewsDetails: React.FC<ScreenProps> = ({ route, navigation }) => {
     const crashlytics = useCrashlytics();
     const { auth: { currentUser } } = useAuth();
 
+    const relativeDate = formatDistanceToNow(parseISO(newsDetails?.publishedAt), { addSuffix: true })
+    const defaultAvatar = `https://api.dicebear.com/7.x/shapes/png?seed=${newsDetails.source.name}`
 
     useEffect(() => {
         if(!newsDetails) {
@@ -37,36 +42,56 @@ const NewsDetails: React.FC<ScreenProps> = ({ route, navigation }) => {
         <Page>
             <Pressable 
                 onPress={() => navigation.goBack()}
-            style={styles.backButton}>
-                <Icon
+                style={styles.backButton}
+            >
+                <Ionicons
                     name="arrow-back"
                     size={20}
                     color="#fff"
                 />
             </Pressable>
+
+            <Text style={styles.heading}>
+                {newsDetails.title}
+            </Text>
+
+            <Text style={[styles.text, styles.subtitle]}>
+                Published {relativeDate}
+            </Text>
+
+            <View style={styles.section}>
+                <Avatar 
+                    avatarStyle={{ borderRadius: 999, width: 40, height: 40 }}
+                    containerStyle={{ width: 40, height: 40 }}
+                    source={{ uri: defaultAvatar }}
+                />
+
+                <View style={{  marginLeft: 7 }}>
+                    <Text>
+                        Published by
+                    </Text>
+
+                    <Text style={[styles.subtitle]}>
+                        {newsDetails.source.name}
+                    </Text>
+                </View>
+            </View>
+
+            <View style={styles.textBlock}>
+                <Text style={styles.mediumText}>
+                    {newsDetails.description}
+                </Text>
+                <Text style={styles.mediumText}>
+                    {newsDetails.content}
+                </Text>
+            </View>
+
             <Image
                 source={{ uri: newsDetails?.urlToImage }}
                 style={styles.newsImage}
                 height={200}
             />
 
-            <View style={styles.section}>
-                <Text style={styles.heading}>
-                    {newsDetails.title}
-                </Text>
-                <Text>
-                    {newsDetails.source.name}
-                </Text>
-            </View>
-
-            <View style={styles.section}>
-                <Text>
-                    {newsDetails.description}
-                </Text>
-                <Text>
-                    {newsDetails.content}
-                </Text>
-            </View>
         </Page>
     )
 }
@@ -80,9 +105,8 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "green",
-        color: "white",
-        marginVertical: 10
+        backgroundColor: "#121212",
+        marginVertical: 12
     },
     newsImage: {
         borderRadius: 12,
@@ -90,11 +114,30 @@ const styles = StyleSheet.create({
     },
     section: {
         width: "100%",
-        marginVertical: 10
+        marginVertical: 10,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center"
     },
     heading: {
-        fontSize: 20,
-        fontWeight: "700"
+        fontSize: 22,
+        fontFamily: "EncodeSans-SemiBold",
+        marginBottom: 10
+    },
+    text: {
+        marginVertical: 5
+    },
+    textBlock: {
+        marginVertical: 10,
+        fontFamily: "EncodeSans-SemiBold"
+    },
+    subtitle: {
+        fontSize: 14,
+        fontFamily: "EncodeSans-Medium"
+    },
+    mediumText: {
+        fontSize: 14,
+        fontFamily: "EncodeSans-Medium"
     }
 })
 
