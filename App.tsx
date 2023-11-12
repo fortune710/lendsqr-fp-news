2962a96866a3371823e5150d23f0930864233938
fortune710 <fortune0208@yahoo.com>
2023-11-13 00:49:01 +0100

    removed useless imports

--- App.tsx
+++ App.tsx
@@ -6,31 +6,18 @@
  */
 
 import React, { useEffect } from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import StackNavigator from './navigation/StackNavigator';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import store from './store';
-import ErrorBoundary from 'react-native-error-boundary';
-import useCrashlytics from './hooks/useCrashlytics';
-import ErrorPage from './components/Error';
 import CodePush from 'react-native-code-push';
 import useRemoteConfig from './hooks/useRemoteConfig';
 
