3f7f0f629ba550c96d42016e4872f70f5159a36a
fortune710 <fortune0208@yahoo.com>
2023-11-13 00:37:38 +0100

    added error boundary to page component

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,19 +1,34 @@
 import { ScrollView } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import useCrashlytics from "../hooks/useCrashlytics"
+import ErrorBoundary from "react-native-error-boundary"
+import ErrorPage from "./Error"
 
 interface PageProps {
     children: React.ReactNode
 }
 
 const Page: React.FC<PageProps> = ({ children }) => {
+
+  const crashlytics = useCrashlytics();
+  
+  const reportError = (error: Error, stackTrace: string) => {
+    crashlytics.log("An error occured")
+    crashlytics.recordError(error, stackTrace)
+  }
+
+
     return (
-        <SafeAreaView>
-            <ScrollView
-                contentContainerStyle={{ paddingHorizontal: 12 }}
-            >
-                { children }
-            </ScrollView>
-        </SafeAreaView>
+        <ErrorBoundary FallbackComponent={ErrorPage} onError={reportError}>
+            <SafeAreaView>
+                <ScrollView
+                    contentContainerStyle={{ paddingHorizontal: 12 }}
+                >
+                    { children }
+                </ScrollView>
+            </SafeAreaView>        
+        </ErrorBoundary>
+
     )
 }
 
